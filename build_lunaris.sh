db662b7eda834f8c56bf206ef458fe02ccd6ca5a
SATHIYA NARAYANAN T <25bai042@hicas.ac.in>
2026-07-22 06:43:10 +0530

    Update revision for oneplus vendor avalon to default

--- build_lunaris.sh
+++ build_lunaris.sh
@@ -89,7 +89,7 @@ cat > .repo/local_manifests/lunaris_avalon.xml << 'LOCALMANIFEST'
   <project name="proprietary_vendor_oneplus_avalon"
            path="vendor/oneplus/avalon"
            remote="sathiya"
-           revision="pong" />
+           revision="lineage-23.2" />
   <project name="proprietary_vendor_oneplus_sm8650-common"
            path="vendor/oneplus/sm8650-common"
            remote="sathiya"
