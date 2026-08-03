f56a53f4965c66bab0e91f9951dca12d85f2dfe7
SATHIYA NARAYANAN T <25bai042@hicas.ac.in>
2026-08-03 07:25:14 +0530

    Update revision for frameworks_base project

--- build_lunaris.sh
+++ build_lunaris.sh
@@ -134,7 +134,7 @@ cat > .repo/local_manifests/lunaris_avalon.xml << 'LOCALMANIFEST'
   <project name="frameworks_base" 
            path="frameworks/base" 
            remote="sathiya" 
-           revision="16.2-bkp" 
+           revision="16.2" 
            clone-depth="1" />
 </manifest>
 LOCALMANIFEST
